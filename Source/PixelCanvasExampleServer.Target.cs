bab5bece349bf33f69629ee71b8fb7e0b140c941
Lokteen Yu <l0kt33n@users.noreply.github.com>
2025-09-27 11:50:34 -0700

    chore: upgrade to Unreal Engine 5.5 and dependencies (#2)
    * fixed engine version in uproject
    
    * Removed all submodules
    
    * removed submodules
    
    * 5.3 template
    
    * removed unused files
    
    * updated server target version
    
    * 5.5 template

--- Source/PixelCanvasExampleServer.Target.cs
+++ Source/PixelCanvasExampleServer.Target.cs
@@ -7,10 +7,11 @@ using System.Collections.Generic;
 public class PixelCanvasExampleServerTarget : TargetRules //Change this line according to the name of your project
 {
 
-	public PixelCanvasExampleServerTarget(TargetInfo Target) : base(Target) //Change this line according to the name of your project
-	{
-		Type = TargetType.Server;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
-		ExtraModuleNames.Add("PixelCanvasExample"); //Change this line according to the name of your project
-	}
+    public PixelCanvasExampleServerTarget(TargetInfo Target) : base(Target) //Change this line according to the name of your project
+    {
+        Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+        ExtraModuleNames.Add("PixelCanvasExample"); //Change this line according to the name of your project
+    }
 }
